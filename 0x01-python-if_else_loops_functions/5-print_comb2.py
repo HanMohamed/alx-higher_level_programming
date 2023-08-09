fb3ace484b7d7c81af9a215d25b375228d2a8d83
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-09 16:41:00 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i == 99:
-        print("{:0>2}".format(i), end='')
-    else:
-        print("{:0>2}".format(i), end=', ')
+    print("{:0>2}".format(i), end=', ')
 print()
