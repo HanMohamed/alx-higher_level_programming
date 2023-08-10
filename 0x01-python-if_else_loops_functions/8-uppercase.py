ae211d65af430579d085acea169daf9b8424deba
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-09 21:41:04 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def uppercase(str):
+    STR = ""
+    for i in str:
+        if ord(i) in range(97, 123):
+            x = ord(i) - 32
+            STR += chr(x)
+        else:
+            STR += i
+    print(STR)
