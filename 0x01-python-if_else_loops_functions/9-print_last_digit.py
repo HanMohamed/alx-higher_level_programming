631ef37d9caf6066f81f2eabd380a2892787ea63
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-09 21:51:23 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def print_last_digit(number):
+    if number < 0:
+        digit = number % -10
+        digit *= -1
+    else:
+        digit = number % 10
+    print("{}".format(digit), end='')
+    return digit
