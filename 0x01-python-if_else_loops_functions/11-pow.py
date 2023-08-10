741755da33ec76fd6b3f63224c8ffa7c89826b6b
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-10 11:47:52 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,16 +2,4 @@
 
 def pow(a, b):
 
-    if b == 0:
-        return 1
-
-    if b < 0:
-        a = 1 / a
-        b = b * -1
-
-    result = a
-    while b > 1:
-        result = result * a
-        b = b - 1
-
-    return result
+    return a ** b
