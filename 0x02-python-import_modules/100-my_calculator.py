4fe485b5caa014f0f6718544905044ade96989e6
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-11 01:32:09 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+if len(sys.argv != 4):
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+a = int(sys.argv[2])
+b = int(sys.argv[4])
+sign = str(sys.argv[3])
+
+match sign:
+    case "+":
+        print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    case "-":
+        print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    case "*":
+        print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    case "/":
+        print("{0} / {1} = {2}".format(a, b, div(a, b)))
