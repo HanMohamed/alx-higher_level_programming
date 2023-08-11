69a5236be1382f57102501c923271012c4fc4673
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-11 00:07:32 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+num = len(sys.argv) - 1
+sum = 0
+
+for i in range(0, num):
+    sum += int(sys.argv[i + 1])
+print("{}".format(sum))
