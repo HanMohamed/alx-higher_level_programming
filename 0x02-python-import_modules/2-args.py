f9bbb87fe379e6c64de66c6e551e01667f52ad15
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-10 23:31:52 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+num = len(sys.argv) - 1
+if (num == 0):
+    print("{} arguments.".format(num))
+else:
+    print("{} arguments:".format(num))
+    for i in range(0, num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
