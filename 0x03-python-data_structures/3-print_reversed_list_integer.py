471c5320d3447ebb33b6b24bc9c0bb2f08d98dae
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-14 16:57:00 -0700

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list)
+    if i == 0:
+        return
     for j in range(len(my_list)):
         print("{:d}".format(my_list[i - 1]))
         i = i - 1
