38418902a923f3e26a8e78b81b4e623326067043
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-15 14:41:26 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,10 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return None
 
-    copy_list = my_list
+    copy_list = []
+    for i in range(len(my_list)):
+        copy_list.append(my_list[i])
+
     if (idx < 0 or idx >= len(my_list)):
         return copy_list
 
