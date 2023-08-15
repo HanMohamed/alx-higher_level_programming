0b15fd53111278837ad2f59301488f180799cee7
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-15 15:03:31 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if my_list is None:
+        return None
+
+    max_integer = my_list[0]
+    for i in range(len(my_list)):
+        if (my_list[i] > max_integer):
+            max_integer = my_list[i]
+
+    return max_integer
