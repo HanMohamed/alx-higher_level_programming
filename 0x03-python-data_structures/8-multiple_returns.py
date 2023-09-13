547a405dae898640fedc56865ca568e4d9018dc5
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-13 14:42:57 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    len_char = ()
-    if (len(sentence) == 0):
-        len_char = len(sentence), None
-    else:
-        len_char = len(sentence), sentence[0]
-    return (len_char)
+    if (sentence):
+        return (len(sentence), sentence[0])
+
+    return (0, None)
