ce0fdef93b2f09e68bc920d8ea0dc2a66291705f
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-13 12:35:25 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) and len(tuple_b) >= 2:
-        return (tuple_a[0]+ tuple_b[0],
+        return (tuple_a[0] + tuple_b[0],
                 tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) or len(tuple_b) == 1: 
-       return (tuple_a[0]+ tuple_b[0],
+    elif len(tuple_a) or len(tuple_b) == 1:
+        return (tuple_a[0] + tuple_b[0],
                 tuple_a[1] + 0)
     else:
         return tuple_a
