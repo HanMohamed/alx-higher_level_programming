be93c4be2440c4d7e3eca3a11ab1de8ecab04798
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-13 16:03:42 -0700

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+{
+	int len_list, i;
+
+	for (len_list = 0; p; len_list++)
+		;
+	printf("[*] Size of the Python List = %d\n", len_list);
+	printf("[*] Allocated = %d\n", len_list);
+	for (i = 0; p; i++)
+		printf("Element %d: %s\n", i, Py_TYPE(p));
+}
