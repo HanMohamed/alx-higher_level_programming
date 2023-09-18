f12ed9e4945f7b1eba1a141389707b47f84b0849
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-18 15:35:15 -0700

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_multiplies_a = {}
+    for key in a_dictionary:
+       new_multiplies_a[key] = a_dictionary[key] * 2
+
+    return new_multiplies_a
