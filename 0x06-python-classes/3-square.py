31fed7ff9ca7d8869ba1e2ac0ab84463b00c6b53
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-27 18:54:11 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """ Define a square by size.
+
+        Args:
+            size: size of square.
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculate square area
+
+            Returns:
+                square area
+        """
+        return (self.__size * self.__size)
