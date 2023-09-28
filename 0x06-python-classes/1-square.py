f1c16ddf7a1d6c5b64e5e2472e1e2db7facb6ce2
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-27 17:15:54 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Define a square class"""
+
+
+class Square:
+    """ Define a square by size 
+    
+        Args:
+            size: size of square.
+    """
+    
+    def __init__(self, size):
+        self.__size = size
