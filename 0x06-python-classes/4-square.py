3c55513248985c0e2a371adb70087f71f4fa6a28
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-27 19:13:50 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,14 +9,9 @@ class Square:
             size: size of square.
     """
     def __init__(self, size=0):
+        self.__size = size
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
+    @property
     def size(self):
         """ Retrieve size
             Returns:
@@ -25,6 +20,7 @@ class Square:
 
         return (self.__size)
 
+    @size.setter
     def size(self, value):
         """A setter function to set size of square"""
 
