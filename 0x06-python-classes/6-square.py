0ff2b40289f9cbc45be2dcc5ef9977722e841129
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-27 20:05:01 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,10 +72,11 @@ class Square:
         if (self.__size == 0):
             print("")
         else:
+            for i in range(0, self.__position[1]):
+                print("")
             for i in range(0, self.__size):
                 for j in range(0, self.__position[0]):
-                    if self.__position[1] == 0:
-                        print(" ", end="")
+                    print(" ", end="")
                 for j in range(0, self.__size):
                     print("#", end="")
                 print("")
