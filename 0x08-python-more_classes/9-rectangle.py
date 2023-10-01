3db57de52d1e36864de695f9693943333f66d392
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-01 12:15:08 -0700

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,6 +12,11 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @classmethod
+    def square(cls, size=0):
+        """ Returns a new instance of Rectangle """
+        return cls(size, size)
+
     def __init__(self, width=0, height=0):
         """ Initialize a new Rectangle.
 
@@ -105,7 +110,3 @@ class Rectangle:
         else:
             return rect_2
 
-    @classmethod
-    def square(cls, size=0):
-        """ Returns a new instance of Rectangle """
-        return cls(size, size)
