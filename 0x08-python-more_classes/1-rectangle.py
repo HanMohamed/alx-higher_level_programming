1dc7614f1d2a774e68a29b4af882674a317fb176
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-01 12:33:38 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
-"""Define a square class"""
+"""
+Defines a class Rectangle
+"""
 
 
 class Rectangle:
-    """ Define a square by size.
-
-        Args:
-            size: size of square.
-    """
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-
-        self.__width = width
-        self.__height = height
+        """Initializes the rectangle"""
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """ Retrieve width
-            Returns:
-                width of rectangle
-        """
-        return (self.__width)
+        """getter for the private instance attribute width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """A setter function to set width of rectangle"""
-
+        """setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """ Retrieve height
-            Returns:
-                height of rectangle
-        """
-        return (self.__height)
+        """getter for the private instance attribute height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """A setter function to set height of rectangle"""
-
+        """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
