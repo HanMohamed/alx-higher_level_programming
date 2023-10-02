0b77ab4857f83de76f4e37f3fd5276d792fb11cf
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-02 13:59:48 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+   A class Square that inherits from BaseGeometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square inherits from 9-rectangle """
+
+    def __init__(self, size):
+        """Instantiate private instance size"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
