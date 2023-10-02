9facb8966310a617a98ec9c5ce66222f862e6d82
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-02 13:55:24 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+    A class Rectangle that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        class Rectangle inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """ private instantiation of attributes """
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
