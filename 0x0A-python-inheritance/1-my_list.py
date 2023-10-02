16e8a154fda9c4c2ab40e27cc09026be64f24613
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-02 13:34:56 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,9 @@
 """my list Function"""
 
 
+class MyList(list):
+    """a class MyList that inherits from list"""
+
 def print_sorted(self):
     """ prints the list, but sorted (ascending sort) """
-    return (self)
+    print(sorted(self))
