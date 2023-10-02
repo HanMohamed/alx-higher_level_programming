44316cd237447fd513ed8b1c3995b8ebdd48b54d
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-02 13:56:48 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+    A class Rectangle that inherits from BaseGeometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+        class Rectangle inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """ private instantiation of attributes """
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """ Return rectangle description width and height """
+
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+
+    def area(self):
+        """ Returns area of Rectangle instance """
+
+        return (self.__width * self.__height)
