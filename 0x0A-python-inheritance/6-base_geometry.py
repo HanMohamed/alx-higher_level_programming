6533e03bb538d1b3526e5a290137e8d3dad83804
Han Mohamed <hmohamed2@seattleu.edu>
2023-10-02 13:50:12 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Base Geometry class"""
+
+
+class BaseGeometry:
+    """A class with public attribute area"""
+
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
