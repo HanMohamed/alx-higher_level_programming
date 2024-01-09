93d7cc83b31bc3d381019318fe20eea94b28ffa4
HanMohamed <hmohamed2@seattleu.edu>
2024-01-08 16:49:43 -0800

    3-value_argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const startArg = 2;
+
+if (!process.argv[startArg]) {
+  console.log('No argument');
+} else {
+  process.argv.forEach(val => {
+    console.log(`${val}`);
+  });
+}
