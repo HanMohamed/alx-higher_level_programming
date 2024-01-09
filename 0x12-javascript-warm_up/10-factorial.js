354a9f54fa1c2df60229b127f90b2471ac2908a5
HanMohamed <hmohamed2@seattleu.edu>
2024-01-09 11:17:36 -0800

    10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+function factorial (a) {
+  if (a === 1) {
+    return (1);
+  }
+  return (a * factorial(a - 1));
+}
+
+const a = parseInt(process.argv[2]);
+
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorial(a));
+}
