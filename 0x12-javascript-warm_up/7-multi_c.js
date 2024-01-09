57ad0f536affb9574a8a2b6154dcfe4f378cbc49
HanMohamed <hmohamed2@seattleu.edu>
2024-01-08 21:31:51 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const multiLang = 'C is fun';
+const times = process.argv[2];
+
+if (!Number.isInteger(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log(multiLang);
+  }
+}
