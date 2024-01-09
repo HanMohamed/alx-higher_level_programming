0da7cf7b2e38210498c18d3cc0bfbf62ac0b05f5
HanMohamed <hmohamed2@seattleu.edu>
2024-01-08 16:58:32 -0800

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,6 +2,6 @@
 
 const conc1 = process.argv[2];
 const conc2 = process.argv[3];
-const stat = ' is ';
+const writeIs = ' is ';
 
-console.log(conc1 + stat + conc2);
+console.log(conc1 + writeIs + conc2);
