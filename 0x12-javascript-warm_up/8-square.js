6d473bb006da20081d559c36d14e94bf16d7ce8a
HanMohamed <hmohamed2@seattleu.edu>
2024-01-08 23:02:47 -0800

    8-square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const square = 'X';
+const times = process.argv[2];
+let squareLength = '';
+
+if (isNaN(times)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < times; i++) {
+    squareLength = '';
+    for (let j = 0; j < times; j++) {
+      squareLength += square;
+    }
+    console.log(squareLength);
+  }
+}
