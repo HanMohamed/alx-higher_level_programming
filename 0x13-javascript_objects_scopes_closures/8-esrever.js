eb4c6359586776076133446b1e3113ff47fb42a8
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 11:33:26 -0800

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+exports.esrever = function (list) {
+  const revList = [];
+
+  for (let j = list.length - 1, i = 0; j >= 0; j--, i++) {
+    revList[i] = list[j];
+  }
+  return (revList);
+};
