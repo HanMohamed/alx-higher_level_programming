867aa1ccbc5bc458c17180c09189b8b9dc28e0a6
HanMohamed <hmohamed2@seattleu.edu>
2024-01-09 20:32:27 -0800

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    let str;
+    for (let i = 0; i < this.height; i++) {
+      str = 'X';
+      for (let j = 0; j < (this.width - 1); j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
