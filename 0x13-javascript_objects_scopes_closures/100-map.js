7c259fe966339bc7d10f8bcc301c5b0b29075286
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 14:25:26 -0800

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const list = require('./100-data').list;
+
+const newList = [];
+let i = 0;
+
+list.forEach(element => {
+  newList[i] = i * element;
+  i++;
+});
+
+console.log(list);
+console.log(newList);
