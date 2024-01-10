5936aa6130aa5ab525eb0205730b7d2d8d8ae049
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 15:54:30 -0800

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const file = new File([fileA, fileB], fileC);
+// fs.writeFile(fileC, '');
+let content;
+
+content = fs.readFileSync(fileA, 'utf8');
+content += '\n';
+content += fs.readFileSync(fileB, 'utf8');
+
+fs.writeFile(fileC, content, function (err) {
+  if (err) throw err;
+});
