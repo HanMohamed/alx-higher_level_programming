4d808f931b4cd04b99703e4b01f7f74567c9e50d
HanMohamed <hmohamed2@seattleu.edu>
2024-01-09 20:37:07 -0800

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    let str;
+    for (let i = 0; i < this.height; i++) {
+      str = 'X';
+      for (let j = 0; j < (this.width - 1); j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const widthTemp = this.width;
+    this.width = this.height;
+    this.height = widthTemp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
