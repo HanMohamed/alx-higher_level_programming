71afa028e90b6ecbe422c6b54013a5a891d49613
HanMohamed <hmohamed2@seattleu.edu>
2024-01-09 21:03:45 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    if (c !== 'C') {
+      super.print();
+    } else {
+      let str;
+      for (let i = 0; i < this.height; i++) {
+        str = 'C';
+        for (let j = 0; j < (this.width - 1); j++) {
+          str += 'C';
+        }
+        console.log(str);
+      }
+    }
+  }
+}
+
+module.exports = Square;
