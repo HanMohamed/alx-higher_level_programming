a1746ce634deb9670b1ec37e45d2935de1da804e
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 14:07:51 -0800

    10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+/*
+ * convert base 10 to any otherbase:
+ *    number.tostring(any base);
+ * convert any otherbase t0 base 10:
+ *    parseInt(number, its base);
+ * convert any base1 to any base2:
+ *    1- numberBase_10 = parseInt(number, base1);
+ *    2- numberNewBase = numberBase_10.toString(base2);
+ */
+
+exports.converter = function (base) {
+  return function (numb) {
+    return (numb.toString(base));
+  };
+};
