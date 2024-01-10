8392d586accf4d3fca806af0526498566b0dbcc8
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 11:17:53 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let numb = 0;
+  list.forEach(element => {
+    numb += element === searchElement ? 1 : 0;
+  });
+  return (numb);
+};
