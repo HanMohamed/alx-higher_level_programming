ec8266c2f8f2d2335f0d9571c27405101ca5a6f8
HanMohamed <hmohamed2@seattleu.edu>
2024-01-10 21:46:10 -0800

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,4 +2,16 @@
 
 const dict = require('./101-data').dict;
 
-const sortedDict = [];
+const sortedDict = {};
+let value;
+
+for (const key in dict) {
+  value = dict[key];
+
+  if (!sortedDict[value]) {
+    sortedDict[value] = [];
+  }
+  sortedDict[value].push(key);
+}
+
+console.log(sortedDict);
