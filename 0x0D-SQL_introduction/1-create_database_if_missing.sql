fc613497085e3338504c52d05a18b4cb1374b7ee
HanMohamed <hmohamed2@seattleu.edu>
2024-01-11 00:11:37 -0800

    2-remove_database.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
