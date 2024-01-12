032d29038730773a327d54d7e8db0ea9c9cbf19f
HanMohamed <hmohamed2@seattleu.edu>
2024-01-11 16:24:03 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table(
   name VARCHAR(256),
   scire INT
 );
-INSERT INTO second_table
+INSERT INTO IF EXISTS second_table
 VALUES
   (1, 'John', 10),
   (2, 'Alex', 3),
