44b41798ca55506bd279198cb20e27652cb894f6
HanMohamed <hmohamed2@seattleu.edu>
2024-01-16 14:48:12 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@
 --    name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
   id INT UNIQUE AUTO_INCREMENT NOT NULL,
