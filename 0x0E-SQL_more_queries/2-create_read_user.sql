3f7116be1704263248bb7687b0936a174f3e0798
HanMohamed <hmohamed2@seattleu.edu>
2024-01-16 13:05:22 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,10 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
-GRANT SELECT PRIVILEGES ON hbtn_0d_2s TO user_0d_2;
-ALTER USER user_0d_2 IDENTIFIER BY user_0d_2_pwd;
+
+CREATE USER IF NOT EXISTS user_0d_2@localhost
+IDENTIFIED BY user_0d_2_pwd;
+
+GRANT SELECT PRIVILEGES 
+ON hbtn_0d_2s 
+TO user_0d_2;
