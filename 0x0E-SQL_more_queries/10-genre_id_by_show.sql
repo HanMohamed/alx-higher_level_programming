edaf8e4a0191805887f0984e9ede3fdc1fc2d019
HanMohamed <hmohamed2@seattleu.edu>
2024-01-16 16:06:33 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -7,6 +7,7 @@
 -- tv_shows(id, title);
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+FROM tv_shows
+  JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_ids;
