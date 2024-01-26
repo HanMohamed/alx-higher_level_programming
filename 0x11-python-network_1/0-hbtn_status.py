7664e88c22d7b6bb5fa404e2ff77056e3024c22e
Hanmohamed2 <hanniaatef@gmail.com>
2024-01-26 14:26:11 -0800

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
