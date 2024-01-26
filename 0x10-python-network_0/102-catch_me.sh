cc15bc56c88120e997ff4003f8d90a0d2479936f
HanMohamed <hmohamed2@seattleu.edu>
2024-01-25 18:07:37 -0800

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cURL body size #curl -s 0.0.0.0:5000/catch_me -o /dev/null -w '%{http_code}'
-curl -sL -X PUT -d '{You got me!}' 0.0.0.0:5000/catch_mes
+curl -sL -X PUT -d '{You got me!}' 0.0.0.0:5000/catch_me
