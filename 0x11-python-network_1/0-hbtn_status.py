70dd67d228043481d97b55de80543a3599522770
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 14:35:02 -0800

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
+    print(html)
