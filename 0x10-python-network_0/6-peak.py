b04d6c76d60d6187a6be4d25a3e4ba66e1f29f20
HanMohamed <hmohamed2@seattleu.edu>
2024-01-25 18:37:30 -0800

    0x10-python-network_0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ find_peak """
 
-def find_peak(list_of_integers): {
 
+def find_peak(list_of_integers):
+    """find_peak
+    Param:
+        list_of_integers
+    """
+    if list_of_integers:
+        list_of_integers.sort()
+        return (list_of_integers[-1])
 
-}
+    return (None)
