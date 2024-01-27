33a2855721eb73249888399629625325da62b5ce
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 16:53:52 -0800

    0x11-python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,13 +2,14 @@
 """ Task:
 """
 import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     value = {'email' : sys.argv[2]}
     byte_value = urllib.parse.urlencode(value)
     byte_value = byte_value.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], value)
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    req = urllib.request.Request(sys.argv[1], byte_value)
+    with urllib.request.urlopen(req) as response:
         html = response.read()
-        print(html)
+        print(html.decode('utf-8'))
