d656309e2e14e0e7e964b5ca8a8ac1b8e7e7f119
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 16:58:13 -0800

    0x11-python-network_1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,10 +6,14 @@ import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    value = {'email' : sys.argv[2]}
+    value = {'email': sys.argv[2]}
+    # data should be parsed first to be bytes
+    byte_value = urllib.parse.urlencode(value)
     # data should be bytes
-    byte_value = value.encode('ascii')
+    byte_value = byte_value.encode('ascii')
+    # passing arg means 'POST'
     req = urllib.request.Request(sys.argv[1], byte_value)
     with urllib.request.urlopen(req) as response:
         html = response.read()
-        print(html.decode('utf-8'))
+        # the response (decoded in utf-8)
+        print(html)
