0b9e946d5d209a2c3b553b93a5f648b53273d23a
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 17:59:16 -0800

    0x11-python-network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     try:
       req = requests.get(sys.argv[1])
     except requests.error.HTTPError as response:
-        print('Error code: {}'.format(response.code))
+        print('Error code: {}'.format(response))
     else:
         print(req.text)
