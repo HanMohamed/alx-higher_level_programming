f82b1e4b14a2e96c6eb4d57fb3f37cbc11fbf184
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 17:53:16 -0800

    0x11-python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" Task: POST an email #0
+""" Task: POST an email #1
 """
 import requests
 import sys
 
 if __name__ == "__main__":
     value = {'email': sys.argv[2]}
-    # data should be parsed first to be bytes
-    # byte_value = requests.parse.urlencode(value)
-    # data should be bytes
-    byte_value = value.encode('ascii')
-    # passing arg means 'POST'
-    req = requests.Request(sys.argv[1], byte_value)
-    response = requests.get(req)
-    html = response.read()
-    # the response (decoded in utf-8)
-    print(html.decode('utf-8'))
+    response = requests.post(sys.argv[1], value)
+    html = response.text
+    print(html)
