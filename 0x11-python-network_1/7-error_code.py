2b15d66da373fc198c0bd2cfb23916913e83df12
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 17:57:27 -0800

    0x11-python-network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" Task:
+""" Error code #0
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    req = requests.get(sys.argv[1])
-    print(req)
+    try:
+      req = requests.get(sys.argv[1])
+    except requests.error.HTTPError as response:
+        print('Error code: {}'.format(response.code))
+    else:
+        print(req.text)
