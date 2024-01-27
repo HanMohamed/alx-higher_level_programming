06ca42f4c85116d7ab59dfae69b34b2715abd43c
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 17:10:26 -0800

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
-    try: 
+    try:
         urllib.request.urlopen(req)
     except urllib.error.HTTPError as response:
-          print('Error code: {}'.format(response.code))
+        print('Error code: {}'.format(response.code))
