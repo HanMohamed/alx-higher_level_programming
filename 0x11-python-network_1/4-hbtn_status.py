bfcacf01e294228cd0eb60be4fd1dfb2c5ea8873
HanMohamed <hmohamed2@seattleu.edu>
2024-01-26 17:33:47 -0800

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     req = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(req)))
-    print("\t- content: {}".format(req.content))
+    print("\t- content: {}".format(req.content.decode('utf-8')))
