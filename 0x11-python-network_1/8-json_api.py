eeb16a8a39c378a4175e3336f4e0f12fb0bcc9ed
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 15:14:39 -0800

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-""" Task:
+""" Task: a Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    req = requests.get(sys.argv[1])
-    print(req)
+    link = "http://0.0.0.0:5000/search_user"
+    req = requests.get(link)
+    #req = requests.post(link, data={'key': 'value'})
+
+    #Check response body is JSON formatted
+    if (req.json):
+        if (sys.argv == 1):
+            req = requests.post(link, data={'q': ''})
+            print('No result')
+        elif (isinstance(sys.argv[1],chr) and req.json):
+            req = requests.post(link, data={'q': sys.argv[1]})
+            print("[{}] {}".format(req.content, req.text))
+    else:
+        print('Not a valid JSON')
