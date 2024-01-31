933e423043c98b60d998919ad7024f0672130907
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 16:04:28 -0800

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     if (req.json):
         if (len(sys.argv) == 2 and (sys.argv[1].isalpha())):
             req = requests.post(link, data={'q': sys.argv[1]})
-            print(req)
+            print(req.headers)
             # print(dict(req.text).get('id'))
             # print(dict(req.headers).get('name'))
 
