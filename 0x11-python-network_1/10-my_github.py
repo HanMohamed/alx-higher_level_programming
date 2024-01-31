00f210d5c970e1ceb431a7c7712d10f04ffff049
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 17:32:48 -0800

    0x11-python-network_1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    link = "https://api.github.com/user/"
+    link = "https://api.github.com/user/" + username
     # req = requests.post(link, data={'key': 'value'})
     req = requests.post(link, auth=(username, password))
     response = req.json()
