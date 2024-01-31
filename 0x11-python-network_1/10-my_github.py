0142442ff72f73dabdd7d246ccaf52c94225c104
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 16:52:56 -0800

    0x11-python-network_1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" Task:
+""" Task: .
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    req = requests.get(sys.argv[1])
-    print(req)
+    link = "https://docs.github.com/en/rest/users?apiVersion=2022-11-28"
+    # req = requests.post(link, data={'key': 'value'})
+    username = sys.argv[1]
+    password = sys.argv[2]
+    head = {username, password}
+    req = requests.get(link, headers=head)
+    print("[{}] {}".format(dict(req).get('id')))
