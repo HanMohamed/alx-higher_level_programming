66c23801de4d9528ec0f51e1eb2be984d772e43f
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 16:34:11 -0800

    0x11-python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,18 +8,18 @@ import sys
 
 if __name__ == "__main__":
     link = "http://0.0.0.0:5000/search_user"
-    req = requests.get(link)
     # req = requests.post(link, data={'key': 'value'})
 
     # Check response body is JSON formatted
-    if (req.json):
-        if (len(sys.argv) == 2 and (sys.argv[1].isalpha())):
-            q = sys.argv[1]
-            req = requests.post(link, data={'q': q})
-            print(req.text.find('id'))
+    if (len(sys.argv) == 2 and (sys.argv[1].isalpha())):
+        q = sys.argv[1]
+        req = requests.post(link, data={'q': q})
+        reponse = req.json()
+        print(dict(reponse).get('id'))
             # print(dict(req.headers).get('name'))
 
-        else:
-            print('No result')
     else:
-        print('Not a valid JSON')
+            print('No result')
+    #else:
+    
+    #   print('Not a valid JSON')
