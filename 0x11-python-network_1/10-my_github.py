7402646c5098e121fbac38283c9a3d8bc9545c14
HanMohamed <hmohamed2@seattleu.edu>
2024-01-30 17:03:22 -0800

    0x11-python-network_1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     link = "https://api.github.com/user"
     # req = requests.post(link, data={'key': 'value'})
-    req = requests.get(link, headers={sys.argv[1], sys.argv[2]})
+    req = requests.get(link, data={sys.argv[1], sys.argv[2]})
     print((dict(req).get('id')))
