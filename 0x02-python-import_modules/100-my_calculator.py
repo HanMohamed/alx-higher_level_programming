042bfd7f739f093b1446961e118088d856e9874e
HanMohamed <hmohamed2@seattleu.edu>
2024-02-20 13:50:20 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
+# Write a program that imports all functions from the file calculator_1.py
+# and handles basic operations.
+
+from calculator_1 import add, sub, mul, div
+import sys
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    sign = sys.argv[2]
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-sign = sys.argv[2]
-
-if sign == '+':
-    print("{0} + {1} = {2}".format(a, b, add(a, b)))
-elif sign == '-':
-    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
-elif sign == '*':
-    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
-elif sign == '/':
-    print("{0} / {1} = {2}".format(a, b, div(a, b)))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    if sign == '+':
+        print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    elif sign == '-':
+        print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    elif sign == '*':
+        print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    elif sign == '/':
+        print("{0} / {1} = {2}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+# Write a program that imports the variable a from the file variable_load_5.py
+# and prints its value.
+# You are not allowed to use * for importing or __import__
+# Your code should not be executed when imported
 
 if __name__ == "__main__":
-    import variable_load_5
-
-print("{}".format(variable_load_5.a))
+    from variable_load_5 import a
+    print("{}".format(a))
