a066a9e730b5ff60ff0deb8234742471523a7f29
HanMohamed <hmohamed2@seattleu.edu>
2024-02-20 13:20:21 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -13,7 +13,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    
     for name in names:
-        print(name)
-    
+        if not name.startswith("__"):
+            print(name)
