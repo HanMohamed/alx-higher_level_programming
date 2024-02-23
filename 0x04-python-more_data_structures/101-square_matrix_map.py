9d3fadcc6f8d7f784c0377d2ea1e5f6145ecc5ad
HanMohamed <hmohamed2@seattleu.edu>
2024-02-22 16:27:31 -0800

    0x04-python-more-data_structures

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
+# Write a function that computes the square value
+# of all integers of a matrix using map
+# Your file should be max 3 lines
 def square_matrix_map(matrix=[]):
     return list(map(lambda x: list(map(lambda y: y*y, x)), matrix))
