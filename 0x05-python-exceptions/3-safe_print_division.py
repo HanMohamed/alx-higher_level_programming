1043761cd9ea80f8beb6d0aeb2ec0c5454db5a2f
HanMohamed <hmohamed2@seattleu.edu>
2024-02-27 18:25:59 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+# Write a function that divides 2 integers and prints the result.
+# The result of the division should print on the finally:
+# section preceded by Inside result:
+# Returns the value of the division, otherwise: None
+# You have to use try: / except: / finally:
 def safe_print_division(a, b):
     try:
         result = a / b
