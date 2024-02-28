46d5621e24fbfb19061b56b38843b27e98b36f6d
HanMohamed <hmohamed2@seattleu.edu>
2024-02-28 14:19:26 -0800

    0x06-python-classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-"""Define a square class"""
+""" Write a class Square that defines a square by:
+    - Private instance attribute: size:
+        - property def size(self): to retrieve it
+        - property setter def size(self, value): to set it:
+            - size must be an integer, otherwise raise a TypeError
+              exception with the message size must be an integer
+            - if size is less than 0, raise a ValueError exception
+              with the message size must be >= 0
+    - Instantiation with optional size: def __init__(self, size=0):
+    - Public instance method: def area(self): returns the current square area
+    - Public instance method: def my_print(self): that prints in stdout
+      the square with the character #:
+      if size is equal to 0, print an empty line
+"""
 
 
 class Square:
-    """ Define a square by size.
+    """Represent a Square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
 
         Args:
-            size: size of square.
-    """
-    def __init__(self, size=0):
+            size (int): the size of new square
+        """
+
         self.__size = size
 
     @property
     def size(self):
-        """ Retrieve size
-            Returns:
-                size of square
-        """
+        """ Retrieve size of square"""
 
         return (self.__size)
 
@@ -28,18 +41,16 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
-        """ Calculate square area
+        """Returns area of square"""
 
-            Returns:
-                square area
-        """
         return (self.__size * self.__size)
 
     def my_print(self):
+        """ Printing a square"""
+
         if (self.__size == 0):
             print("")
         else:
