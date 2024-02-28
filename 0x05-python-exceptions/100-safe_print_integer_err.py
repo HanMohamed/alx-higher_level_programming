5d4c8ab1c606674039af245deb208a5014321072
HanMohamed <hmohamed2@seattleu.edu>
2024-02-28 13:04:15 -0800

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+# Write a function that prints an integer.
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
+    else:
+        return True
