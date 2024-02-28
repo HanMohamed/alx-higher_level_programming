0866a5837d1539a3b9872b03db43f973eb2dbf03
HanMohamed <hmohamed2@seattleu.edu>
2024-02-28 13:11:45 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,10 @@ import sys
 
 
 # Write a function that executes a function safely.
+# You can assume fct will be always a pointer to a function
+# Returns the result of the function,
+# Otherwise, returns None if something happens during the function
+# and prints in stderr the error precede by Exception:
 def safe_function(fct, *args):
     try:
         result = fct(*args)
