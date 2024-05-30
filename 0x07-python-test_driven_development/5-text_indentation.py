82ab1a36d67b6818485a34c5c62147a5ad50bd49
HanMohamed <hmohamed2@seattleu.edu>
2024-05-30 16:32:51 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,12 +4,14 @@
 
 def text_indentation(text):
     """Print text with two new lines after each '.', '?', and ':'.
+    There should be no space at the beginning or
+    at the end of each printed line
     Args:
         text (string): The text to print.
     Raises:
         TypeError: If text is not a string.
     """
-    
+
     if type(text) is not str:
         raise TypeError("text must be a string")
 
@@ -22,5 +24,3 @@ def text_indentation(text):
             while i + 1 < len(text) and text[i + 1] == ' ':
                 i += 1
         i += 1
-
-        
