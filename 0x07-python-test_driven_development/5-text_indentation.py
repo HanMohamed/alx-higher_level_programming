369b327dd4149decb343fffd178ccc68953f5b18
HanMohamed <hmohamed2@seattleu.edu>
2024-05-30 15:23:05 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,20 +9,13 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
+    
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    for letter in text:
+        print(letter, end="")
+        if letter in ['.', '?', ':']:
+            print()
+            print()
+        
