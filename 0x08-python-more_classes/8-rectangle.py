f58fbda3561b899009b2ab47370bf78ea6e0ada1
HanMohamed <hmohamed2@seattleu.edu>
2024-06-01 06:43:18 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -11,6 +11,18 @@ class Rectangle:
 
     number_of_instances = 0
     print_symbol = "#"
+    
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ rect_1 and rect_2 must be instance of rectangle class """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
 
     def __init__(self, width=0, height=0):
         """ Initialize a new Rectangle.
@@ -92,15 +104,3 @@ class Rectangle:
         type(self).number_of_instances -= 1
         """Print a message for every deletion of a Rectangle."""
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """ rect_1 and rect_2 must be instance of rectangle class """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
