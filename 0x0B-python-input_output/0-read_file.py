860a32d344761ddbf9f616ae62e0a84c2510ddc0
HanMohamed <hmohamed2@seattleu.edu>
2024-06-23 05:41:23 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """0. Read file"""
 
+
 def read_file(filename=""):
     """a function that reads a text file (UTF8) and prints it to stdout:"""
     with open(filename, encoding="utf-8") as file:
         date = file.read()
+        print(date)
