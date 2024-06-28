b5923ad92c9d17e386b570fa6f86822a5c556f2c
HanMohamed <hmohamed2@seattleu.edu>
2024-06-27 21:20:48 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
-with open(filename, "a", encoding="utf-8") as file:
-    file.write("")
 
-#loaded_file = load_from_json_file(filename)
-#save_to_json_file(loaded_file, filename)
+try:
+    loaded_obj = load_from_json_file(filename)
+except:
+    loaded_obj = []
+    
 for args in sys.argv[1:]:
-    save_to_json_file(args, filename)
+    loaded_obj.append(args)
+
+save_to_json_file(loaded_obj, filename)
