4b2a4eaea8caca8a18226447627277824d3e5133
HanMohamed <hmohamed2@seattleu.edu>
2024-06-27 21:21:54 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""7. Load, add, save
+Write a script that adds all arguments to a Python list,
+and then save them to a file"""
 import sys
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
