7e4a67de7abbddb999f51eb3085a467f8ba1a502
HanMohamed <hmohamed2@seattleu.edu>
2024-06-27 22:07:07 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """8. Class to JSON"""
-import json
 
 
 def class_to_json(obj):
     """Write a function that returns the dictionary description
     with simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object"""
+    return(obj.__dict__)
