c3cd50aa195df8ba3f1c95b44ce79f5b6d8e22d1
HanMohamed <hmohamed2@seattleu.edu>
2024-07-01 15:38:36 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,10 +14,12 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student"""
         to_json_dict = {}
-        if not attrs or attrs is []:
+
+        if attrs is None:
             to_json_dict = self.__dict__
         else:
             for attr in attrs:
                 if attr in self.__dict__:
                     to_json_dict[attr] = self.__dict__[attr]
+
         return to_json_dict
