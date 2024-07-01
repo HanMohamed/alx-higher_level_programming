ca0c43c3c9b47702e897b87075580f57985359b6
HanMohamed <hmohamed2@seattleu.edu>
2024-07-01 14:57:27 -0700

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""9. Student to JSON"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        to_json_dict = {}
+        if not attrs:
+            return self.__dict__
+        else:
+            for attr in attrs:
+                if attr in self.__dict__:
+                    to_json_dict[attr] = self.__dict__[attr]
+        return to_json_dict
