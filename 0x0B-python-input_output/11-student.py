5823a57af67c5b8b92b138396113cd74f415f2ed
HanMohamed <hmohamed2@seattleu.edu>
2024-07-01 15:58:03 -0700

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""11. Student to disk and reload"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        to_json_dict = {}
+
+        if attrs is None:
+            to_json_dict = self.__dict__
+        else:
+            for attr in attrs:
+                if attr in self.__dict__:
+                    to_json_dict[attr] = self.__dict__[attr]
+
+        return to_json_dict
+
+    def reload_from_json(self, json):
+        """eplaces all attributes of the Student instance"""
+
+        for key in json:
+            self.key = json[key]    
